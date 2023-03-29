514c268c366df51be3cfceec893216b057d5a368
Gregory Damiani <gregory.damiani@mobimeo.com>
2023-03-29 16:10:32 +0800

    cert-manager, ingress-nginx, doc update

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
+locals {
+  kubernetes_version = "1.23.4"
+  argocd_namespace   = "argocd"
+}
+
 terraform {
   required_providers {
     kind = {
-      source = "tehcyx/kind"
+      source  = "tehcyx/kind"
       version = "0.0.16"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.1"
     }
   }
 }
@@ -15,17 +24,17 @@ provider "kind" {
 }
 
 resource "kind_cluster" "kindle" {
-  name = "kindle"
-  node_image = "kindest/node:v1.23.4"
+  name           = "kindle"
+  node_image     = "kindest/node:v${local.kubernetes_version}"
   wait_for_ready = "true"
   kind_config {
-    kind = "Cluster"
+    kind        = "Cluster"
     api_version = "kind.x-k8s.io/v1alpha4"
 
     # default address is localhost. define a port to avoid random allocation.
     networking {
       api_server_address = "127.0.0.1"
-      api_server_port = 6443
+      api_server_port    = 6443
       # uncomment this when we're ready to install cilium
       #disable_default_cni = "true"
     }
@@ -33,12 +42,15 @@ resource "kind_cluster" "kindle" {
     node {
       role = "control-plane"
 
-
-      extra_mounts { 
+      extra_mounts {
         host_path      = "./share"
         container_path = "/share"
       }
- 
+
+      kubeadm_config_patches = [
+        "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
+      ]
+
       extra_port_mappings {
         container_port = 80
         host_port      = 8080
@@ -50,7 +62,7 @@ resource "kind_cluster" "kindle" {
     }
     node {
       role = "worker"
-      
+
     }
     node {
       role = "worker"
@@ -71,17 +83,73 @@ provider "kubernetes" {
   config_context = "kind-kindle"
 }
 
-# create a namespace for Argo
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-  }
-}
-
 provider "kubectl" {
   config_path = "./kindle-config"
 }
 
-resource "kubectl_manifest" "my_service" {
-    yaml_body = file("./argocd-install.yaml")
+provider "helm" {
+  kubernetes {
+    config_path = "./kindle-config"
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  chart      = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  name       = "cert-manager"
+
+  create_namespace = "true"
+  namespace        = "cert-manager"
+
+  timeout = 900
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  depends_on = [
+    kind_cluster.kindle
+  ]
+}
+
+resource "helm_release" "ingress_nginx" {
+  name = "ingress-nginx"
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  create_namespace = "true"
+  namespace        = "ingress-nginx"
+
+  wait = true
+  timeout = 900
+
+  depends_on = [
+    helm_release.cert_manager
+  ]
+}
+
+# create a namespace for Argo
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = local.argocd_namespace
+  }
+  depends_on = [
+    helm_release.ingress_nginx
+  ]
+}
+
+# install argocd with a module that leverages the official helm chart - https://github.com/argoproj/argo-helm/tree/main/charts/argo-cd 
+module "argocd" {
+  source               = "aigisuk/argocd/kubernetes"
+  version              = "0.2.7"
+  namespace            = local.argocd_namespace
+  argocd_chart_version = "5.27.4"
+  timeout_seconds      = 900
+  admin_password       = "admin"
+  insecure             = "true"
+  depends_on = [
+    kubernetes_namespace.argocd
+  ]
 }
